db52ba63d7e6991ce400bfddf5875cd508a71daa
nurik030608-jpg <nurik030608@gmail.com>
2026-01-05 23:23:23 +0400

    Update 0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import string
 
+def from_numpy(array):
+    """
+    Создает pd.DataFrame из np.ndarray с заголовками колонок в алфавитном порядке.
+    """
+    # Определяем количество колонок в массиве
+    num_columns = array.shape[1]
+    
+    # Генерируем список букв (A, B, C...) в зависимости от количества колонок
+    column_names = list(string.ascii_uppercase[:num_columns])
+    
+    # Создаем DataFrame
+    df = pd.DataFrame(array, columns=column_names)
+    
+    return df
