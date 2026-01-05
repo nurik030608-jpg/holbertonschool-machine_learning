25642f21758734634b31ef86b0dd52d88b38f6c8
nurik030608-jpg <nurik030608@gmail.com>
2026-01-05 23:32:15 +0400

    Update 2-from_file.py

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that contains the function from_file
+"""
+import pandas as pd
 
+
+def from_file(filename, delimiter):
+    """
+    Loads data from a file as a pd.DataFrame
+    
+    Args:
+        filename: the file to load from
+        delimiter: the column separator
+        
+    Returns:
+        the loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, sep=delimiter)
