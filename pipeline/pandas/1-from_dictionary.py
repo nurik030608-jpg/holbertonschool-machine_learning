bb6f8e6dedda273e03eec71075c047cc6415028c
nurik030608-jpg <nurik030608@gmail.com>
2026-01-05 23:30:47 +0400

    Update 1-from_dictionary.py

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+# Данные для столбцов
+data = {
+    'First': [0.0, 0.5, 1.0, 1.5],
+    'Second': ['one', 'two', 'three', 'four']
+}
+
+# Метки строк
+row_labels = ['A', 'B', 'C', 'D']
+
+# Создание DataFrame
+df = pd.DataFrame(data, index=row_labels)
