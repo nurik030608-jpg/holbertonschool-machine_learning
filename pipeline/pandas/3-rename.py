7eb3ccb2fb1e007a461bb69915e82c7e8d4529d1
nurik030608-jpg <nurik030608@gmail.com>
2026-01-10 19:29:56 +0400

    Update 3-rename.py

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+"""Module to rename a column and convert to datetime"""
 import pandas as pd
+
 
 def rename(df):
     # 1. Rename the 'Timestamp' column to 'Datetime'
     df = df.rename(columns={'Timestamp': 'Datetime'})
-    
+
     # 2. Convert to datetime specifying the unit as seconds ('s')
-    # This corrects the 1970 vs 2019 discrepancy
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
-    
+
     # 3. Filter to display only the Datetime and Close columns
     df = df[['Datetime', 'Close']]
-    
+
     return df
