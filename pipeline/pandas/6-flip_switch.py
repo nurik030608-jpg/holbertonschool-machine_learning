b1481494827f0a539d0473aafacd42a7f37399c0
nurik030608-jpg <nurik030608@gmail.com>
2026-01-10 19:33:20 +0400

    Update 6-flip_switch.py

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
+"""Module to sort and transpose a DataFrame"""
+
+
+def flip_switch(df):
+    """
+    Sorts the data in reverse chronological order and transposes it.
+
+    Args:
+        df: pd.DataFrame to be transformed.
+
+    Returns:
+        The transformed pd.DataFrame.
+    """
+    # 1. Sort the dataframe by index in descending order
+    # 2. Transpose the result using .T
+    df = df.sort_index(ascending=False).T
+
+    return df
+
 
