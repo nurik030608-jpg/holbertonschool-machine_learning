529f6df2490d1c30f1fa32228f22dd730489baed
nurik030608-jpg <nurik030608@gmail.com>
2026-01-11 23:18:04 +0400

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Модуль для построения линейного графика"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Строит график y = x^3 в виде красной сплошной линии"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, 'r-')
