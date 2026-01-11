debcd61b4abb4e583d32b35bd1206737b78e22d4
nurik030608-jpg <nurik030608@gmail.com>
2026-01-11 23:18:45 +0400

    Update 1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
+"""Модуль для построения диаграммы рассеяния"""
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+def scatter():
+    """Строит диаграмму рассеяния зависимости веса от роста"""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.scatter(x, y, c='m')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+
+    plt.show()
