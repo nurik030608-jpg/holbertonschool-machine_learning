dac1fafcec2428955d814549ddfac2adf1baa5a8
nurik030608-jpg <nurik030608@gmail.com>
2026-01-18 17:19:14 +0400

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-"""Module for summation of i squared"""
+"""Module to calculate summation"""
 
 
 def summation_i_squared(n):
     """
-    Calculates the sum of i^2 from 1 to n without using loops.
-    Formula: n(n + 1)(2n + 1) / 6
+    Sum of squares from 1 to n
     """
-    # Strict type check: n must be an integer.
-    # If the checker passes a float, it is usually considered invalid.
     if type(n) is not int or n < 0:
         return None
 
-    # The formula for the sum of the first n squares
-    # Use floor division (//) to ensure an integer return value.
+    # Using formula: n(n + 1)(2n + 1) / 6
     return (n * (n + 1) * (2 * n + 1)) // 6
