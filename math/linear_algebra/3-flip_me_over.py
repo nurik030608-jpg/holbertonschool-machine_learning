80d251fb5cc1d290ce7f4e9bd7ef07f3a596f685
nurik030608-jpg <nurik030608@gmail.com>
2026-01-22 17:28:37 +0400

    Update 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+"""Модуль для транспонирования 2D матрицы"""
 
+
+def matrix_transpose(matrix):
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transpose = [[matrix[i][j] for i in range(rows)] for j in range(cols)]
+    
+    return transpose
