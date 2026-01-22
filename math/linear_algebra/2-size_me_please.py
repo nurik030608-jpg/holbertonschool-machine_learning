bcbbdaee80c9042dd0e4785675ae052d8e212f0b
nurik030608-jpg <nurik030608@gmail.com>
2026-01-22 16:14:24 +0400

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-matrix_shape = __import__('2-size_me_please').matrix_shape
+"""Defines a function to calculate the shape of a matrix"""
+
+
 def matrix_shape(matrix):
-  shape = []
+    """
+    Calculates the shape of a matrix and returns it as a list of integers.
+    
+    Args:
+        matrix: A nested list representing a matrix.
+        
+    Returns:
+        A list of integers representing the dimensions.
+    """
+    shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
         if len(matrix) == 0:
             break
         matrix = matrix[0]
     return shape
-
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
