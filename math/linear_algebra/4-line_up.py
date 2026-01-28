1ccf695bad4f856e346147768c622ad74af16a36
nurik030608-jpg <nurik030608@gmail.com>
2026-01-28 15:38:44 +0400

    Update 4-line_up.py

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+
 def add_arrays(arr1, arr2):
-  if len(arr1) != len(arr2):
-    return none
-  return [x + y for x, y in zip(arr1, arr2)]
-    print(add_arrays(arr1, arr2))        # Ожидаем: [6, 8, 10, 12]
-    print(arr1)                          # Ожидаем: [1, 2, 3, 4] (оригинал не меняется)
-    print(arr2)                          # Ожидаем: [5, 6, 7, 8]
-    print(add_arrays(arr1, [1, 2, 3]))   # Ожидаем: None
+    if len(arr1) != len(arr2):
+        return None  
+    return [x + y for x, y in zip(arr1, arr2)]
+
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+
+print(add_arrays(arr1, arr2))        # [6, 8, 10, 12]
+print(arr1)                          # [1, 2, 3, 4]
+print(arr2)                          # [5, 6, 7, 8]
+print(add_arrays(arr1, [1, 2, 3]))   # None
