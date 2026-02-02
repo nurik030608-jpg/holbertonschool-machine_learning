873f0a514ba4e45d2525d460880ee844beb2fbfd
nurik030608-jpg <nurik030608@gmail.com>
2026-02-02 16:09:44 +0400

    Update 9-let_the_butcher_slice_it.py

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-"""Module to slice a 2D numpy array into specific sub-matrices"""
 import numpy as np
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
