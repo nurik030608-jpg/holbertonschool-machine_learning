5b3c935a65b1cfddbf5e2b2f00930cda5343845b
nurik030608-jpg <nurik030608@gmail.com>
2026-02-02 16:43:28 +0400

    Update 4-line_up.py

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 """
-Этот блок называется docstring. В некоторых проверках он обязателен
-для описания модуля.
+This module provides a function to add two arrays element-wise.
 """
 
 
 def add_arrays(arr1, arr2):
     """
-    Складывает два списка поэлементно.
-    Если формы не совпадают, возвращает None.
+    Adds two arrays element-wise.
+    Returns None if shapes are not equal.
     """
     if len(arr1) != len(arr2):
         return None
-    
-    # Создаем новый список, не меняя старые
+
     return [x + y for x, y in zip(arr1, arr2)]
