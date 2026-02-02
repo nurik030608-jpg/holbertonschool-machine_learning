bd0977fc42328deba36cf101f7bf6daf82319082
nurik030608-jpg <nurik030608@gmail.com>
2026-02-02 16:43:51 +0400

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 """
-Модуль для поэлементного сложения 2D матриц.
+Module for element-wise addition of 2D matrices.
 """
 
+
 def add_matrices2D(mat1, mat2):
-  """
-    Складывает две матрицы 2D.
-    Возвращает новую матрицу или None, если размеры не совпадают.
     """
-  if len(mat1) != len(mat2):
-    return None
-  if len(mat1[0]) != len(mat2[0]):
-    return None
-  return [
+    Adds two 2D matrices.
+    Returns a new matrix or None if dimensions do not match.
+    """
+    if len(mat1) != len(mat2):
+        return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    return [
         [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
         for i in range(len(mat1))
     ]
