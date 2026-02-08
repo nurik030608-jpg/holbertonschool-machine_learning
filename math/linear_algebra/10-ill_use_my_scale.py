ce96fe918e0260b65259cf298ff9872cec5fa3d4
nurik030608-jpg <nurik030608@gmail.com>
2026-02-08 16:46:57 +0400

    Create 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function to calculate the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray as a tuple of integers"""
+    return matrix.shape
