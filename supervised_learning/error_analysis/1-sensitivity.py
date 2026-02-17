e1be3ecf392ceec7620c1a41cdb19b4ccf35ce06
nurik030608-jpg <nurik030608@gmail.com>
2026-02-17 16:48:35 +0400

    Update 1-sensitivity.py

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to calculate sensitivity
+"""
+import numpy as np
 
+
+def sensitivity(confusion):
+    """
+    Calculates the sensitivity for each class in a confusion matrix
+    """
+    tp = np.diag(confusion)
+    actual_positives = np.sum(confusion, axis=1)
+    res = tp / actual_positives
+    return res
