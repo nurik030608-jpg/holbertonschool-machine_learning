5f1388f52b1d68853e47f342fb9038eade00da19
nurik030608-jpg <nurik030608@gmail.com>
2026-02-22 13:46:07 +0400

    Create 3-specificity.py

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Module to calculate the specificity of a confusion matrix
+"""
+import numpy as np
+
+
+def specificity(confusion):
+    """
+    Calculates the specificity for each class in a confusion matrix
+    Args:
+        confusion: numpy.ndarray of shape (classes, classes)
+    Returns:
+        numpy.ndarray of shape (classes,) containing the specificity
+    """
+    # TP is the diagonal of the matrix
+    tp = np.diag(confusion)
+    
+    # FP is the sum of the columns minus the true positives
+    fp = np.sum(confusion, axis=0) - tp
+    
+    # FN is the sum of the rows minus the true positives
+    fn = np.sum(confusion, axis=1) - tp
+    
+    # TN is the total sum minus (TP + FP + FN)
+    # This represents all samples correctly identified as NOT belonging to the class
+    total = np.sum(confusion)
+    tn = total - (tp + fp + fn)
+    
+    # Specificity = TN / (TN + FP)
+    # TN + FP is also equivalent to the total number of actual negative samples
+    specificity_values = tn / (tn + fp)
+    
+    return specificity_values
