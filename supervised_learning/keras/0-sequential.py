f5dceeb5557093c68d166ce396def73a5ae37362
nurik030608-jpg <nurik030608@gmail.com>
2026-03-19 16:14:04 +0400

    Update 0-sequential.py

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,25 +6,20 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network with the Keras library.
-
     Args:
         nx: number of input features
         layers: list containing the number of nodes in each layer
         activations: list containing the activation functions for each layer
         lambtha: L2 regularization parameter
         keep_prob: probability that a node will be kept for dropout
-
     Returns:
         The keras model
     """
     model = K.Sequential()
-    
-    # Define the L2 regularizer
     reg = K.regularizers.l2(lambtha)
 
     for i in range(len(layers)):
         if i == 0:
-            # First layer defines the input_shape (nx,)
             model.add(K.layers.Dense(
                 layers[i],
                 activation=activations[i],
@@ -38,7 +33,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                 kernel_regularizer=reg
             ))
 
-        # Add Dropout after each layer except the last one
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
