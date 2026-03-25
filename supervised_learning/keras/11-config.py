0279e3f9800870f07ef46932d3af59cbca0d3527
nurik030608-jpg <nurik030608@gmail.com>
2026-03-25 18:52:25 +0400

    Create 11-config.py

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Module to save and load model architecture in JSON format
+"""
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """
+    Saves a model's configuration in JSON format.
+
+    Args:
+        network: the model whose configuration should be saved
+        filename: the path of the file that the configuration should be saved to
+    Returns:
+        None
+    """
+    config = network.to_json()
+    with open(filename, 'w') as f:
+        f.write(config)
+
+
+def load_config(filename):
+    """
+    Loads a model with a specific configuration.
+
+    Args:
+        filename: the path of the file containing the model's configuration
+    Returns:
+        The loaded model (without weights)
+    """
+    with open(filename, 'r') as f:
+        config = f.read()
+    return K.models.model_from_json(config)
