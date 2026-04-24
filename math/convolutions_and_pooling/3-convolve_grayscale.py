86dbbeea36b8157b6d086c7058bafaa7b0208876
nurik030608-jpg <nurik030608@gmail.com>
2026-04-24 14:24:02 +0400

    Update 3-convolve_grayscale.py

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -10,41 +10,33 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     Выполняет свертку над набором полутоновых изображений.
 
     Аргументы:
-        images: numpy.ndarray формы (m, h, w).
-        kernel: numpy.ndarray формы (kh, kw).
+        images: numpy.ndarray формы (m, h, w) с изображениями.
+        kernel: numpy.ndarray формы (kh, kw) с ядром.
         padding: кортеж (ph, pw), 'same' или 'valid'.
-        stride: кортеж (sh, sw).
+        stride: кортеж (sh, sw) с шагом свертки.
 
     Возвращает:
-        numpy.ndarray с результатом свертки.
+        numpy.ndarray, содержащий результат свертки.
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
 
-    # Определение параметров padding
+    # 1. Определяем размеры паддинга
     if padding == 'valid':
         ph, pw = 0, 0
     elif padding == 'same':
-        # Формула для 'same' с учетом stride:
-        # ph = ceil(((h - 1) * sh + kh - h) / 2)
-        # Но стандартная реализация "same" обычно ориентируется на stride=1
-        ph = ((h - 1) * sh + kh - h) // 2 + 1
-        pw = ((w - 1) * sw + kw - w) // 2 + 1
+        # Для 'same' рассчитываем паддинг так, чтобы выход был h x w
+        ph = (((h - 1) * sh + kh - h) // 2) + 1
+        pw = (((w - 1) * sw + kw - w) // 2) + 1
     else:
         ph, pw = padding
 
-    # Применяем padding
-    images_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
-                           mode='constant', constant_values=0)
+    # 2. Применяем паддинг к изображениям
+    # Паддинг только по высоте (ось 1) и ширине (ось 2)
+    img_pad = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                     mode='constant', constant_values=0)
 
-    # Вычисляем размеры выходного изображения
-    # Формула: floor((size + 2*p - k) / s) + 1
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
-
-    # Инициализация результата
-    convolved = np.zeros((m, output_h, output_w))
-
-    # Основной цикл свертки (итерируемся по координатам выхода)
-    for i in range(output_
+    # 3. Вычисляем размеры выходного изображения
+    output_h = ((h + 2 * ph - kh) // sh) + 1
+    output_w = ((w +
