86afc639bc86b049cd9ff7d0cb16cfb6dc4dc0d6
nurik030608-jpg <nurik030608@gmail.com>
2026-05-14 17:41:36 +0400

    Update 3-contrast.py

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module to adjust the contrast of an image randomly
+"""
+import tensorflow as tf
 
+
+def change_contrast(image, lower, upper):
+    """
+    Randomly adjusts the contrast of an image.
+
+    Parameters:
+    image: A 3D tf.Tensor representing the input image.
+    lower: A float representing the lower bound of the contrast factor range.
+    upper: A float representing the upper bound of the contrast factor range.
+
+    Returns:
+    The contrast-adjusted image 3D tf.Tensor.
+    """
+    return tf.image.random_contrast(image, lower, upper)
