d004fcd0e8fea90284476e86fb774517276eb551
nurik030608-jpg <nurik030608@gmail.com>
2026-05-14 17:42:35 +0400

    Update 5-hue.py

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module to adjust the hue of an image
+"""
+import tensorflow as tf
 
+
+def change_hue(image, delta):
+    """
+    Changes the hue of an image.
+
+    Parameters:
+    image: A 3D tf.Tensor containing the image to change.
+    delta: A float representing the amount the hue should change.
+           Must be in the interval [-0.5, 0.5].
+
+    Returns:
+    The altered image 3D tf.Tensor.
+    """
+    return tf.image.adjust_hue(image, delta)
