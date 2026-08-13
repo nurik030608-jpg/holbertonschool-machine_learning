7b3b4f6e65f0a637afe98a61331e70755c528918
nurik030608-jpg <nurik030608@gmail.com>
2026-08-13 22:09:26 +0400

    Update 0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -2,7 +2,6 @@
 """Loads the FrozenLake environment from Gymnasium."""
 
 import gymnasium as gym
-from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
@@ -17,9 +16,9 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
       The initialized gymnasium environment.
   """
   if desc is None and map_name is None:
-    desc = generate_random_map(size=8)
+    desc = gym.envs.toy_text.frozen_lake.generate_random_map(size=8)
 
   env = gym.make(
-      "FrozenLake-v1", desc=desc, map_name=map_name, is_slippery=is_slippery
+      'FrozenLake-v1', desc=desc, map_name=map_name, is_slippery=is_slippery
   )
   return env
