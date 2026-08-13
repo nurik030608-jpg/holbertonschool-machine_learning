79f00e34e18384825f5b90e12b82d00380cdff57
nurik030608-jpg <nurik030608@gmail.com>
2026-08-13 22:12:32 +0400

    Create 4-play.py

---
+++ reinforcement_learning/q_learning/4-play.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Loads the FrozenLake environment from Gymnasium."""
+
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+  """Loads the pre-made FrozenLakeEnv environment from gymnasium.
+
+  Args:
+      desc: list of lists containing a custom description of the map, or None.
+      map_name: string containing the pre-made map name to load, or None.
+      is_slippery: boolean determining if the ice is slippery.
+
+  Returns:
+      The initialized gymnasium environment with render_mode="ansi".
+  """
+  if desc is None and map_name is None:
+    map_name = '8x8'
+
+  if desc is not None:
+    env = gym.make(
+        'FrozenLake-v1', desc=desc, is_slippery=is_slippery, render_mode='ansi'
+    )
+  else:
+    env = gym.make(
+        'FrozenLake-v1',
+        map_name=map_name,
+        is_slippery=is_slippery,
+        render_mode='ansi',
+    )
+
+  return env
